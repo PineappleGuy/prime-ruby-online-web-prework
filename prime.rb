c99d8ac7bdc02f43f7a82942ba2627ca7bb29dbe
Garth Lilly <garff247@gmail.com>
2019-04-12 02:00:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,12 @@
 
 
 def prime?(number)
-  x = 2
-  x +=1
-  if x % 2 == 0 || x % 3 == 0 || x % 5 == 0 || x % 7 == 0
+  if number % 2 == 0 || x % 3 == 0 || x % 5 == 0 || x % 7 == 0
     puts false
+    number += 1
 
   else
     puts true
+    number += 1
   end
 end

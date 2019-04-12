2fee05e479a70b548f3b58f66c58454ff56d45a2
Garth Lilly <garff247@gmail.com>
2019-04-12 01:58:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+range = (1..float::INFINITY)
+array = [range]
+def prime?(number)
+  x = 2
+  x +=1
+  if x % 2 == 0 || x % 3 == 0 || x % 5 == 0 || x % 7 == 0
+    puts false
+
+  else
+    puts true
+  end
+end
